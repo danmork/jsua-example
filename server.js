8679fbee79e1451cdc7084d341875783159882b6
Dan Mork <danmork@gmail.com>
2017-03-03 12:23:43 -0600

    Examples for Container and Link

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 
 express.static.mime.define({"application/lynx+json": ["lnx"]});
 express.static.mime.define({"application/lynx-spec+json": ["lnxs"]});
-app.use(express.static("out"));
+
+app.use(express.static("out", { index: [ "index.html", "index.lnx" ] }));
 
 app.listen(3000, function () {
   console.log("JSUA Example app is listening on port 3000...");
