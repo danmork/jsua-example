5612c0605efcf7b7be501bd5dada7eb127e4eb60
Dan Mork <danmork@gmail.com>
2017-03-05 12:33:17 -0600

    Lynx Submit Example

--- src/config.js
+++ src/config.js
@@ -28,7 +28,61 @@ function genericViewBuilder(content) {
   });
 }
 
+function selectDown(view, selector) {
+  var matches = view.querySelectorAll(selector);
+  matches = Array.prototype.slice.call(matches);
+  if (view.matches(selector)) matches.push(view);
+  return matches;
+}
+
+function selectUp(view, selector) {
+  var contentContainer = document.querySelector("#content");
+  var currentView = view;
+  var matches = [];
+  
+  while (currentView !== contentContainer) {
+    if (currentView.matches(selector)) matches.push(currentView);
+    currentView = currentView.parentElement;
+  }
+  
+  return matches;
+}
+
+function lynxLinkClickBehavior(result) {
+  if (result.content.blob.type.indexOf("application/lynx+json") === -1) return;
+  
+  var linkViews = selectDown(result.view, "[data-lynx-hints~=link]");
+  
+  linkViews.forEach(function (linkView) {
+    linkView.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      jsua.fetch(linkView.href);
+    });
+  });
+}
+
+function lynxSubmitClickBehavior(result) {
+  if (result.content.blob.type.indexOf("application/lynx+json") === -1) return;
+  
+  var submitViews = selectDown(result.view, "[data-lynx-hints~=submit]");
+  
+  submitViews.forEach(function (submitView) {
+    submitView.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      var options = {};
+      // TODO: build FormData object
+      if (submitView.formMethod) options.method = submitView.formMethod;
+      if (submitView.formEnctype) options.enctype = submitView.formEnctype;
+      jsua.fetch(submitView.formAction, options);
+    });
+  });
+}
+
 views.attaching.setIsAttached(isAttached);
 views.attaching.register("root-attacher", rootAttacher);
 views.building.register("application/lynx+json", lynx.building.build);
 views.building.register("*/*", genericViewBuilder);
+views.finishing.register("lynx.link.behavior", lynxLinkClickBehavior);
+views.finishing.register("lynx.submit.behavior", lynxSubmitClickBehavior);
